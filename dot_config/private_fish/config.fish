e9771b2009c272924d6a876d5a1c1f4569e5c1e9
Sam Nooij <samnooij@protonmail.com>
2026-02-14 11:45:07 +0100

    Add FISH config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+### bling.fish source start
+test -f /usr/share/ublue-os/bling/bling.fish && source /usr/share/ublue-os/bling/bling.fish
+### bling.fish source end
+
+set -x EZA_ICONS_AUTO
